e490acf5ad798de27e7e60b1f9138fab3ba50063
Tianqi <ta85032@gmail.com>
2024-01-01 07:23:43 +0300

    123

---
+++ Server/Data/edge.js
@@ -0,0 +1,167 @@
+export default [{
+    id:"e1-2",
+    source:"1",
+    target:"2",
+    type:"smoothstep"
+},
+{
+    id:"e1-3",
+    source:"1",
+    target:"3",
+    type:"smoothstep"
+},
+{
+    id:"e1-4",
+    source:"1",
+    target:"4",
+    type:"smoothstep"
+},
+{
+    id:"e2-a",
+    source:"2",
+    target:"a",
+    type:"smoothstep",
+    label:"mean",
+    animated:true
+},
+{
+    id:"e2-5",
+    source:"2",
+    target:"5",
+    type:"smoothstep"
+},
+{
+    id:"e3-b",
+    source:"3",
+    target:"b",
+    type:"smoothstep",
+    label:"mean",
+    animated:true
+},
+{
+    id:"e4-c",
+    source:"4",
+    target:"c",
+    type:"smoothstep",
+    label:"mean",
+    animated:true
+},
+{
+    id:"e5-d",
+    source:"5",
+    target:"d",
+    type:"smoothstep",
+    label:"mean",
+    animated:true
+},
+{
+    id:"e5-6",
+    source:"5",
+    target:"6",
+    type:"smoothstep"
+},
+{
+    id:"e5-7",
+    source:"5",
+    target:"7",
+    type:"smoothstep"
+},
+{
+    id:"e5-8",
+    source:"5",
+    target:"8",
+    type:"smoothstep"
+},{
+    id:"e3-9",
+    source:"3",
+    target:"9",
+    type:"smoothstep"
+},
+{
+    id:"e3-10",
+    source:"3",
+    target:"10",
+    type:"smoothstep"
+},
+{
+    id:"e3-11",
+    source:"3",
+    target:"11",
+    type:"smoothstep"
+},
+{
+    id:"e4-14",
+    source:"4",
+    target:"14",
+    type:"smoothstep"
+},
+{
+    id:"e14-e",
+    source:"14",
+    target:"e",
+    type:"smoothstep",
+    label:"mean",
+    animated:true,
+    
+},
+{
+    id:"e4-15",
+    source:"4",
+    target:"15",
+    type:"smoothstep"
+},
+{
+    id:"e15-f",
+    source:"15",
+    target:"f",
+    type:"smoothstep",
+    label:"mean",
+    animated:true,
+},
+{
+    id:"e15-16",
+    source:"15",
+    target:"16",
+    type:"smoothstep",
+  
+},
+{
+    id:"e16-g",
+    source:"16",
+    target:"g",
+    type:"smoothstep",
+    label:"mean",
+    animated:true,
+  
+},
+{
+    id:"e16-18",
+    source:"16",
+    target:"18",
+    type:"smoothstep",
+    
+  
+},
+{
+    id:"e15-17",
+    source:"15",
+    target:"17",
+    type:"smoothstep",  
+},
+{
+    id:"e17-h",
+    source:"17",
+    target:"h",
+    type:"smoothstep",
+    label:"mean",
+    animated:true,
+  
+},{
+    id:"e17-19",
+    source:"17",
+    target:"19",
+    type:"smoothstep",
+    
+  
+},
+]
